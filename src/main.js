514a7aa4a78082d92f0ba83e64332e44c9f44b97
mrliuwd <jidanm00004@163.com>
2019-11-01 19:22:44 +0800

    添加vuex

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import ViewUI from 'view-design';
+import ViewUI from 'view-design'
+
 import 'view-design/dist/styles/iview.css';
 Vue.config.productionTip = false
 Vue.use(ViewUI);
